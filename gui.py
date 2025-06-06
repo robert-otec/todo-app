28b1bd3bc7347ecd1ec23055a79b8c43ea35a9a2
robert-otec <robertotec@outlook.cz>
2025-06-06 14:23:16 +0200

    add file creation if it doesn't exist

--- gui.py
+++ gui.py
@@ -1,8 +1,12 @@
 import modules.functions
 import FreeSimpleGUI as sg
 import time
+import os
 
-
+if not os.path.exists("todos.txt"):
+    with open("todos.txt", "w") as file:
+        pass
+        
 sg.theme("Black")
 
 
