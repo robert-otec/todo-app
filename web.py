114d143cc33fa301224786bd4e249f237ccbf969
robert-otec <robertotec@outlook.cz>
2025-06-09 11:36:43 +0200

    Implement Add new todo feature

--- web.py
+++ web.py
@@ -3,11 +3,21 @@ import modules.functions
 
 todos = modules.functions.get_todos()
 
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    modules.functions.write_todos(todos)
+    st.session_state["new_todo"] = ""  # Clear the input box after adding
+
+
 st.title("My Todo App")
 st.subheader("A simple todo app using Streamlit")
-st.write("This is a simple todo app that allows you to add, edit, and complete todos.")
+st.write("A simple todo app that allows you to add, edit, and complete todos.")
 
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input("Add a new todo:", placeholder="Enter a new todo here...")
+st.text_input("Add a new todo:", placeholder="Enter a new todo here...",
+              on_change=add_todo, key="new_todo")
+
+st.session_state
