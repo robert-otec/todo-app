3ed5d96f57f5ceaa9e60174e37af279503d843aa
robert-otec <robertotec@outlook.cz>
2025-06-09 11:49:53 +0200

    Add complete function to the web app

--- web.py
+++ web.py
@@ -12,12 +12,15 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("A simple todo app using Streamlit")
-st.write("A simple todo app that allows you to add, edit, and complete todos.")
+st.write("A simple todo app that allows you to add and complete todos.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        modules.functions.write_todos(todos)
+        del st.session_state[todo]  # Remove the checkbox state
+        st.rerun()
 
 st.text_input("Add a new todo:", placeholder="Enter a new todo here...",
               on_change=add_todo, key="new_todo")
-
-st.session_state
